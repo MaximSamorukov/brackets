3d1dbba9ffd7133599934b2420228a87df818f89
Maxim Samorukov <maxim.samorukov@gmail.com>
2020-09-28 21:32:36 +0300

    update js

--- src/index.js
+++ src/index.js
@@ -2,97 +2,137 @@ module.exports = function check(str, bracketsConfig) {
   // your solution
   const string = str;
   const brConf = bracketsConfig;
-  let nextOption = [];
+  const strArray = string.split('');
 
-  function ifIsOpen(bracket) {
-    console.log(`8: ${bracket}`);
-    let returnValue = '';
-    let a = brConf.filter((i) => {
-      console.log(`11: ${i}`);
-      console.log(`12: ${i.includes(bracket)}`);
-      return i.includes(bracket);
-    })[0][0] === bracket;
-    console.log(`15: ${a}`);
-    return a;
+  function isEqualWithOpposite(str) {
+    const result = brConf.filter((i) => i.includes(str))[0].filter((i) => i === str).length;
+    return result === 2;
   }
 
-  function getOposite(bracket) {
-    let returnValue = '';
-    returnValue = brConf.filter((i) => i.includes(bracket)).filter((i) => i !== bracket)[0];
-    return returnValue;
+  function elementsAreEqual(a, b) {
+    return a === b;
   }
 
-  function getAllOpenBrackets() {
-
+  function returnOpposite(str) {
+    const result = brConf.filter((i) => i.includes(str))[0].filter((i) => i !== str);
+    // console.log(result)
+    return result[0];
   }
 
-  function nextStepOptions(bracket = '') {
+  function ifBracketIsOpen(str) {
+    const result = brConf.filter((i) => i.includes(str))[0][0] === str;
+    return result;
+  }
 
-    let returnArray = [];
-    if (bracket === '') {
-      brConf.map((i) => {
-        console.log(`27: ${i}`);
-        console.log(`28: ${i[0]}`);
-        returnArray.push(i[0]);
-        console.log(`30: ${returnArray}`);
-        return i;
-      })
-    } else {
-      brConf.map((i) => {
-        console.log(`35: ${i}`);
-        console.log(`36: ${i[0]}`);
-        returnArray.push(i[0]);
-        console.log(`38: ${returnArray}`);
-        return i;
-      });
-      returnArray.push(getOposite(bracket));
-    };
-    console.log(`43: ${returnArray}`);
-    console.log(`44: ${returnArray instanceof Array}`);
-    return returnArray;
-  };
-
-  nextOption.push(nextStepOptions());
-  console.log(`48: ${nextOption}`);
-  console.log(`49: ${str}`);
-  let strArray = str.split('');
-  console.log(`50: ${strArray}`);
-  strArray = strArray.map((i, index, arr) => {
-    console.log(`54: ${i}; ${typeof i}`);
-    console.log(`53: ${nextOption}`);
-    console.log(nextOption.length);
-    console.log(`56: ${nextOption[nextOption.length - 1]}; ${nextOption[nextOption.length - 1] instanceof Array}`);
-    console.log(`55: ${nextOption[nextOption.length - 1].map((ii, index) => {
-      console.log(`66: ${ii}`);
-      console.log(`index: ${index}`);
-      console.log(`68: ${ii.includes(i)}`);
-      return i;
-    })}`);
-    if (nextOption[nextOption.length - 1].filter((ii) => ii.includes(i)).length !== 0) {
-      if (ifIsOpen(i)) {
-        console.log(`${i} is open`);
-        nextOption.push(nextStepOptions(i));
-        console.log(`69: ${nextOption}`);
-      } else {
-        console.log(`${i} is not open`)
-        nextOption.pop();
+  function bracketSolver(arr) {
+    let lastIndex = 0;
+    let count = 0;
+    let innerChunk = '';
+    let firstElement = arr[0];
+    let lastElement = '';
+    return arr.map((i, index, arry) => {
+      if (arry.length < 2) {
+        return false;
       }
-      return true;
-    } else {
-      return false;
-    }
-    return i;
-  });
-  console.log(`80: ${strArray}`);
-  console.log(`87: ${nextOption}`);
-  console.log(`87: ${nextOption.length}`);
-  nextOption.map((i) => {
+      console.log(`i: ${i}; index: ${index}; array: ${arry}`);
+
+      if (index === 0 && isEqualWithOpposite(i)) {
+        lastIndex = arr.lastIndexOf(i);
+        count += 1;
+        return true;
+      }
+      if (index !== 0 && isEqualWithOpposite(firstElement) && index !== lastIndex) {
+        count += 1;
+        innerChunk += i;
+        return true;
+      }
+      if (index !== 0 && isEqualWithOpposite(firstElement) && index === lastIndex) {
+        count += 1;
+        const n = innerChunk;
+        console.log(`48: ${n}`);
+        innerChunk = '';
+        if (n.split('').length > 0) {
+          return bracketSolver(n.split(''));
+        } else {
+          return true;
+        }
+
+      }
+      if (index !== 0 && isEqualWithOpposite(firstElement) && index === arry.length - 1 && (count % 2 !== 0)) {
+
+        return false;
+      }
+
+      if (index === 0 && ifBracketIsOpen(i)) {
+        count += 1;
+        console.log(`35 count: ${count}`);
+        return true;
+      }
+
+      else if (index === 0 && !ifBracketIsOpen(i)) {
+        console.log(`40`);
+        return false;
+      };
+      if (index !== 0 && (i[0] === firstElement)) {
+        count += 1;
+      }
+      else if (index !== 0 && (i[0] === returnOpposite(firstElement))) {
+
+        count -= 1;
+        console.log(`47 count: ${count}`);
+        if (count === 0) {
+          const retV = innerChunk;
+          innerChunk = '';
+          if (index !== arry.length - 1) {
+            firstElement = arry[index + 1];
+            if (!ifBracketIsOpen(firstElement)) {
+              return false;
+            }
+          }
+
+          if (retV.length === 0) {
+            return true;
+          } else {
+            return bracketSolver(retV.split(''));
+          }
+
+        }
+        else if (count > 0) {
+          innerChunk += i;
+        } else {
+          console.log(`58`);
+          return false;
+        }
+      }
+      if (index === arry.length - 1 && (i !== returnOpposite(firstElement))) {
+        console.log(`62 i: ${i}`);
+        console.log(`62': ${returnOpposite(firstElement)}`);
+        console.log(i !== returnOpposite(firstElement));
+
+        console.log(`63`);
+        return false;
+
+      }
+      if (count === 0 && (i === firstElement) && index !== 0) {
+
+        count += 1;
+      }
+      if (index !== 0) {
+        return i;
+      }
+
+    })
+
+  }
+
+  let ss = bracketSolver(str.split(''));
+  console.log(`ss: ${ss}`);
+  ss = ss.filter((i) => {
     console.log(i);
-    return i;
+    console.log(i === false);
+    return (i !== true);
   });
-  return strArray.filter((i) => {
-    // console.log(`88: ${i}`);
-    // console.log(`89: ${i === true}`);
-    return (i === true);
-  }).length === strArray.length && nextOption.length === 1;
+  console.log(`ss: ${ss}`);
+  return ss.length === 0;
+  console.log(bracketSolver(str.split('')));
 }
